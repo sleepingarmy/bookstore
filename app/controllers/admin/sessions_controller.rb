1aac8caab8abe44ab9c9232a2c510e81b6b04b07
Rosemary Thomas <rsm.thomas@gmail.com>
2015-06-09 16:37:28 -0600

    made create and destroy actions in sessions controller.

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -5,10 +5,18 @@ class Admin::SessionsController < ApplicationController
   end
 
   def create
-
+    @admin = Admin.where(:username params[:username]).first
+      if @admin.present? && @admin.authenticate(params[:password])
+        session[:admin_id] = @admin.id
+        flash[:notice] = 'Welcome!'
+        redirect_to admin_path
+      else
+        flash[:alert] 'Oops, you entered the wrong username or password. wah wah.'
+        render :new
   end
 
   def destroy
-    
+    session[:user_id] = nil
+    redirect_to admin_login_path
   end
 end
